10ed9ce02b7e6ff373050ad3f093d2b769f7ca6f
Felipe Anzolin <felipedofd@hotmail.com>
2021-09-28 19:12:54 -0300

    Update ListViewMoldel.kt

--- app/src/main/java/com/example/ganjawiki/ListViewMoldel.kt
+++ app/src/main/java/com/example/ganjawiki/ListViewMoldel.kt
@@ -2,8 +2,29 @@ package com.example.ganjawiki
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.ganjawiki.data.RetrofitInitializer
+import com.example.ganjawiki.data.StrainResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class ListViewMoldel : ViewModel() {
-    val ganja = MutableLiveData<ListItems>()
+    val ganja = MutableLiveData<StrainResponse>()
+    private val ganjaApi = RetrofitInitializer.ganjaApi()
 
-}
+    fun setStrain(sort : String) {
+
+        val result = ganjaApi.getStrains(count = 1000, sort).enqueue(object : Callback<StrainResponse>{
+            override fun onResponse(call: Call<StrainResponse>, response: Response<StrainResponse>) {
+                ganja.postValue(response.body())
+            }
+
+            override fun onFailure(call: Call<StrainResponse>, t: Throwable) {
+                print(t.toString())
+            }
+
+        })
+        }
+
+    }
+
