4ae688e784d68b9cefdd91cfca482251438afbec
Felipe Anzolin <felipedofd@hotmail.com>
2021-09-28 19:12:48 -0300

    Update Genetics.kt

--- app/src/main/java/com/example/ganjawiki/data/Genetics.kt
+++ app/src/main/java/com/example/ganjawiki/data/Genetics.kt
@@ -2,16 +2,6 @@ package com.example.ganjawiki.data
 
 import com.google.gson.annotations.SerializedName
 
-data class Genetics (@SerializedName("name") val name : String,
-                     @SerializedName("ocpc") val ocpc : String,
-                     @SerializedName("qr") val qr : String,
-                     @SerializedName("url") val url : String,
-                     @SerializedName("image") val image : String,
-                     @SerializedName("seedCompany") val seedCompany : SeedCompany,
-                     @SerializedName("genetics") val genetics : Genetics,
-                     @SerializedName("lineage") val lineage : Lineage,
-                     @SerializedName("children") val children : Boolean,
-                     @SerializedName("createdAt") val createdAt : String,
-                     @SerializedName("updatedAt") val updatedAt : String)
-
+data class Genetics (@SerializedName("names") val names : String,
+                     @SerializedName("ocpc") val ocpc : String)
 
