7b849c259a68058401613f00f7865e9442e01dd8
Felipe Anzolin <felipedofd@hotmail.com>
2021-09-29 16:24:18 -0300

    Update StrainResponse.kt

--- app/src/main/java/com/example/ganjawiki/data/StrainResponse.kt
+++ app/src/main/java/com/example/ganjawiki/data/StrainResponse.kt
@@ -2,7 +2,6 @@ package com.example.ganjawiki.data
 
 import com.google.gson.annotations.SerializedName
 
-data class StrainResponse (@SerializedName("data") val data : List<Data>,
-                           @SerializedName("meta") val meta : Meta)
+data class StrainResponse (@SerializedName("data") val data : List<Data>)
 
 
