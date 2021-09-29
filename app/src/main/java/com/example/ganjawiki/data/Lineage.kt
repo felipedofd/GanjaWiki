cf549849185bd852fe85838771dd294982a8a77c
Felipe Anzolin <felipedofd@hotmail.com>
2021-09-29 16:24:27 -0300

    Delete Lineage.kt

--- app/src/main/java/com/example/ganjawiki/data/Lineage.kt
+++
@@ -1,8 +0,0 @@
-package com.example.ganjawiki.data
-
-import com.google.gson.annotations.SerializedName
-
-data class Lineage (@SerializedName("Afghanistan") val afghanistan : String,
-                    @SerializedName("South Africa") val southAfrica : String)
-
-
