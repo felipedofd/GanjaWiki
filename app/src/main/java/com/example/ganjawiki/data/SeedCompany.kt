e39a35e0478ea52756fc6b140b61a33ce6790c3f
Felipe Anzolin <felipedofd@hotmail.com>
2021-09-29 16:24:20 -0300

    Delete SeedCompany.kt

--- app/src/main/java/com/example/ganjawiki/data/SeedCompany.kt
+++
@@ -1,8 +0,0 @@
-package com.example.ganjawiki.data
-
-import com.google.gson.annotations.SerializedName
-
-data class SeedCompany (@SerializedName("name") val name : String,
-                        @SerializedName("ocpc") val ocpc : String)
-
-
