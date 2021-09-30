25c1903bd63ac6563c939799b6df81128a68b9ee
Felipe Anzolin <felipedofd@hotmail.com>
2021-09-30 18:04:47 -0300

    Update MyApp.kt

--- app/src/main/java/com/example/ganjawiki/MyApp.kt
+++ app/src/main/java/com/example/ganjawiki/MyApp.kt
@@ -1,18 +1,22 @@
 package com.example.ganjawiki
 
 import android.app.Application
+import com.example.ganjawiki.data.di.apiModule
 import com.example.ganjawiki.data.di.mainModule
+import com.example.ganjawiki.data.di.retrofitModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 
 class MyApp : Application() {
     override fun onCreate() {
         super.onCreate()
 
         startKoin {
-            androidLogger()
+            androidLogger(Level.DEBUG)
             androidContext(this@MyApp)
+            modules(listOf(mainModule, apiModule, retrofitModule))
 
             modules(mainModule)
         }
